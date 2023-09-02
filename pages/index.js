d21bac6499ecbb37a6d2f68416959a6397cb2962
Igor <igor.paes88@gmail.com>
2023-09-02 00:40:25 -0300

    correcao

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
+function Contador() {
+
+    const [contador,setContador] = useState(1);
+
+    function ClickContador(){
+        setContador(contador + 1)
+    }
+
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={ClickContador}>Clique</button>
+        </div>
+    )
+}
+
 function Home() {
     return (
     <div>
